2f2f1536a442f566c2f24d9ce6c0b74aabca886b
mauricioclasta <mauricioclasta@tjro.jus.br>
2025-08-21 22:58:46 -0400

    Mudando tecla de start

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ import time
 
 def main():
     print("Copie um texto com Ctrl+C, depois clique onde quiser digitar.")
-    print("Quando estiver pronto, pressione Ctrl+Shift+T para começar.")
-    keyboard.wait("ctrl+shift+t")
+    print("Quando estiver pronto, pressione F9 para começar.")
+    keyboard.wait("f9")
     time.sleep(1.5)
     run_typing()
 
